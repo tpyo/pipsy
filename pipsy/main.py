f0a2e2e21caf6e327840ee154d7cc5dc817b0e74
Donovan Schönknecht <don@tpyo.net>
2017-04-18 00:42:25 +0200

    Update CLI flags

--- pipsy/main.py
+++ pipsy/main.py
@@ -9,7 +9,7 @@ def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('packages', metavar='packages', type=str, nargs='*',
                         help='Packages to check, if not supplied, all installed packages will be checked')
-    parser.add_argument('-c', '--changelog-security-check', dest='check_changelogs', action='store_true', default=False,
+    parser.add_argument('-c', '--check-changelogs', dest='check_changelogs', action='store_true', default=False,
                         help='Check changelogs for possible security notices')
     parser.add_argument('-j', '--output-json', dest='output_json', action='store_true', default=False,
                         help='Output results as JSON')
